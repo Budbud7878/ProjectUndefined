2e90d90f24cd16a7246b8a779043b4952e6670fa
vazqud1 <140456040+vazqud1@users.noreply.github.com>
2024-06-10 13:08:58 -0700

    Player finally rotates to the position of the mouse
    Finally making progress but its still slow, need you guys' help for some of what I left off, specifically the raycast for attack range (More info inside player script in a comment)

--- Assets/Assets/Scripts/Player Controller.cs
+++ Assets/Assets/Scripts/Player Controller.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Camera playerCam;
+
+    public GameObject player;
+
     public float moveSpeed;
     public float sprintSpeed;
+    [SerializeField] private float rayLength;
     [SerializeField] private float attackRange;
+
+    private Plane groundPlane;
+
+    private Ray camRay;
+    private Ray attackRay;
+
+    private Vector3 pointToLook;
+    private Vector3 rangeOfAttack;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
@@ -25,12 +40,22 @@ public class PlayerController : MonoBehaviour
 
         //Update the GameObject's position with the detected move direction and speed.
         transform.position += moveDirection * speed * Time.deltaTime;
-    }
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Vector3 direction = transform.forward * attackRange;
-        Gizmos.DrawRay(transform.position, direction);
+        groundPlane = new Plane(Vector3.up, Vector3.zero);      //Sets the ground plane
+
+        camRay = playerCam.ScreenPointToRay(Input.mousePosition);   //Makes the ray point in the direction of the mouse.
+
+        rangeOfAttack = transform.forward * attackRange;    /* Could use help with this one Boris, or Eder if you can, basically I was thinking of using this little calculation
+                                                            to create raycast that moves from the players Z axis till it reaches a set range but im stumped, I need this ray because 
+                                                              it will be used to detect whether an enemy is in range to get hit by an attack or not so yeah */
+
+        Debug.DrawRay(camRay.origin, camRay.direction * rayLength, Color.red);  //Creates a visual Representation of the raycast.
+
+        if (groundPlane.Raycast(camRay, out rayLength))     //Forbids the ray from going beyond the set ground plane.
+        {
+            pointToLook = camRay.GetPoint(rayLength);
+        }
+
+        transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));  //Makes player game object rotate in the direction of the raycast.
     }
 }
