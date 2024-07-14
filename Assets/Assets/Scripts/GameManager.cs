8d462754a8bc464e541e74146c308c3ac0b9429c
Budbud7878 <borisramosdiaz@icloud.com>
2024-07-14 03:36:11 -0400

    Added a camera and fixed a bug
    Just added a simple camera needs much much work tho. Can't rotate nor do other features that I have in mind. I'll see if i work on that tmr. ITS LATE OKAY

--- Assets/Assets/Scripts/GameManager.cs
+++ Assets/Assets/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private CinemachineVirtualCamera MainCamera;
     void Start()
     {
-        
+        // Ignore this debug.logs() adding them for structure... sort off. (I like to type stuff it let's my mind work faster so I typed this as warm up.)
+        Debug.Log("GameManager loading...");
+
+
+        Debug.Log("GameManager's has finished all initial tasks.");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
--- Assets/Assets/Scripts/Player Controller.cs
+++ Assets/Assets/Scripts/Player Controller.cs
@@ -64,7 +64,6 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerCam = GameObject.Find("Main Camera").GetComponent<Camera>();
         player = GameObject.Find("Player");
 
         enemyLayer = LayerMask.GetMask("Enemy");
