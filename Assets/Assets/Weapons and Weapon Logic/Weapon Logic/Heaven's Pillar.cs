40bdc03f980beeb4822f746491e23d9bd124505e
Budbud7878 <borisramosdiaz@icloud.com>
2024-07-20 03:53:58 -0400

    fixed a small naming error
    Be careful when creating new scripts and giving them names with apostrophes. I fixed it tho, also didn't get the chance to clock in today nor yesterday. I'll see if I work on the main branch for the inventory system.

--- Assets/Assets/Weapons and Weapon Logic/Weapon Logic/Heaven's Pillar.cs
+++ Assets/Assets/Weapons and Weapon Logic/Weapon Logic/Heaven's Pillar.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Heaven'sPillar : MonoBehaviour
+public class HeavensPillar : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Assets/Weapons and Weapon Logic/Weapon Logic/LumberJack's Rage.cs
+++ Assets/Assets/Weapons and Weapon Logic/Weapon Logic/LumberJack's Rage.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LumberJack'sRage : MonoBehaviour
+public class LumberJacksRage : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
