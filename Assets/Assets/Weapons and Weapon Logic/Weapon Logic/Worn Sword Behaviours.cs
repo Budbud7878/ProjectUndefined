2886b35357fde78920d2e5be1f578247eaae6193
vazqud1 <140456040+vazqud1@users.noreply.github.com>
2024-07-23 13:05:58 -0700

    another

--- Assets/Assets/Weapons and Weapon Logic/Weapon Logic/Worn Sword Behaviours.cs
+++ Assets/Assets/Weapons and Weapon Logic/Weapon Logic/Worn Sword Behaviours.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WornSword : MonoBehaviour
+public class WornSwordBehaviours : MonoBehaviour
 {
-
-    
-
     // Start is called before the first frame update
     void Start()
     {
