783c8f9ba0a8b47b63718c270e3e6ff63cde5052
Budbud7878 <borisramosdiaz@icloud.com>
2024-07-26 19:39:34 -0400

    Revert "added some stuff"
    This reverts commit f850422f41a3aeb75fdc56c293fe5c7d07fb678c.

--- Assets/Assets/General Scripts/WeaponDataStorage.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WeaponDataStorage : MonoBehaviour
-{
-    [SerializeField] private List<GameObject> Weapons = new List<GameObject>();
-}
